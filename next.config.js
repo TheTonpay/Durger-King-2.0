b4e52a7a99fbc5b840b2116bf5655559980eacd9
Arterialist <arterialist@thetonpay.app>
2023-03-15 17:30:55 +0400

    configure next.js for pages, replace workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? "/Durger-King-2.0/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
